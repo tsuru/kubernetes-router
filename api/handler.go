84e569becfc17fb12553242f342198f17b4941b1
André Carvalho <asantostc@gmail.com>
2017-09-05 17:34:35 -0300

    api: move api logic to separated pkg

---
+++ api/handler.go
@@ -0,0 +1,19 @@
+// Copyright 2017 tsuru authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package api
+
+import "net/http"
+
+type handler func(http.ResponseWriter, *http.Request) error
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	handleError(h(w, r), w, r)
+}
+
+func handleError(err error, w http.ResponseWriter, r *http.Request) {
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
