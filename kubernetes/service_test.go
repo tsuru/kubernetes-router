833bd8c7b6206f4a8c772b63396cbc9c1f93b3c8
André Carvalho <asantostc@gmail.com>
2017-12-08 16:09:48 -0200

    kubernetes: ignore headless services

--- kubernetes/service_test.go
+++ kubernetes/service_test.go
@@ -11,23 +11,24 @@ import (
 	"github.com/tsuru/kubernetes-router/router"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	apiv1 "k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/kubernetes/fake"
+	"k8s.io/client-go/pkg/api/v1"
 )
 
 func TestAddresses(t *testing.T) {
-	svc1 := apiv1.Service{ObjectMeta: metav1.ObjectMeta{
+	svc1 := v1.Service{ObjectMeta: metav1.ObjectMeta{
 		Name:      "test",
 		Namespace: "default",
 		Labels:    map[string]string{appLabel: "test", appPoolLabel: "pool"},
 	},
-		Spec: apiv1.ServiceSpec{
-			Ports: []apiv1.ServicePort{{Protocol: "TCP", Port: int32(8899), TargetPort: intstr.FromInt(8899), NodePort: 9090}},
+		Spec: v1.ServiceSpec{
+			Ports: []v1.ServicePort{{Protocol: "TCP", Port: int32(8899), TargetPort: intstr.FromInt(8899), NodePort: 9090}},
 		},
 	}
-	node := apiv1.Node{ObjectMeta: metav1.ObjectMeta{
+	node := v1.Node{ObjectMeta: metav1.ObjectMeta{
 		Labels: map[string]string{poolLabel: "pool"},
 	},
-		Status: apiv1.NodeStatus{Addresses: []apiv1.NodeAddress{{Type: apiv1.NodeInternalIP, Address: "192.168.10.1"}}},
+		Status: v1.NodeStatus{Addresses: []v1.NodeAddress{{Type: v1.NodeInternalIP, Address: "192.168.10.1"}}},
 	}
 	svc := createFakeService()
 	err := svc.Create("test", router.Opts{})
@@ -50,5 +51,64 @@ func TestAddresses(t *testing.T) {
 	}
 	if !reflect.DeepEqual(addresses, expected) {
 		t.Errorf("Expected %v. Got %v.", expected, addresses)
+	}
+}
+
+func TestGetWebService(t *testing.T) {
+	headlessSvc := v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-headless",
+			Namespace: "default",
+			Labels:    map[string]string{appLabel: "test", headlessServiceLabel: "true"},
+		},
+		Spec: v1.ServiceSpec{
+			Selector: map[string]string{"name": "test"},
+		},
+	}
+	svc := BaseService{Namespace: "default", Client: fake.NewSimpleClientset()}
+	_, err := svc.Client.CoreV1().Services(svc.Namespace).Create(&headlessSvc)
+	if err != nil {
+		t.Errorf("Expected err to be nil. Got %v.", err)
+	}
+	webService, err := svc.getWebService("test")
+	expectedErr := ErrNoService{App: "test"}
+	if err != expectedErr {
+		t.Errorf("Expected err to be %v. Got %v. Got service: %v", expectedErr, err, webService)
+	}
+	svc1 := v1.Service{ObjectMeta: metav1.ObjectMeta{
+		Name:      "test-single",
+		Namespace: "default",
+		Labels:    map[string]string{appLabel: "test"},
+	},
+		Spec: v1.ServiceSpec{
+			Selector: map[string]string{"name": "test-single"},
+			Ports:    []v1.ServicePort{{Protocol: "TCP", Port: int32(8899), TargetPort: intstr.FromInt(8899)}},
+		},
+	}
+	_, err = svc.Client.CoreV1().Services(svc.Namespace).Create(&svc1)
+	if err != nil {
+		t.Errorf("Expected err to be nil. Got %v.", err)
+	}
+	svc2 := v1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "test-web",
+			Namespace: "default",
+			Labels:    map[string]string{appLabel: "test", processLabel: "web"},
+		},
+		Spec: v1.ServiceSpec{
+			Selector: map[string]string{"name": "test-web"},
+			Ports:    []v1.ServicePort{{Protocol: "TCP", Port: int32(8890), TargetPort: intstr.FromInt(8890)}},
+		},
+	}
+	_, err = svc.Client.CoreV1().Services(svc.Namespace).Create(&svc2)
+	if err != nil {
+		t.Errorf("Expected err to be nil. Got %v.", err)
+	}
+	webService, err = svc.getWebService("test")
+	if err != nil {
+		t.Errorf("Expected err to be nil. Got %v.", err)
+	}
+	if webService.Name != "test-web" {
+		t.Errorf("Expected service to be %v. Got %v.", svc2, webService)
 	}
 }
